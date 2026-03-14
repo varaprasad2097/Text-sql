e60f033f3f8b8fcaa45b1f636603058ec8e71677
varaprasad2097 <135033358+varaprasad2097@users.noreply.github.com>
2026-03-15 01:02:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain_chroma import Chroma
